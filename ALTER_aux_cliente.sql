3074dc3985a338d9e7e15d37f2683a00f648c83c
dvargas89 <136857446+dvargas89@users.noreply.github.com>
2024-02-28 14:45:28 -0500

    Add files via upload

---
+++ ALTER_aux_cliente.sql
@@ -0,0 +1,37 @@
+ALTER TABLE kace2_belcorp.aux_clientes ADD cod_pais varchar(2) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD cod_campania varchar(2) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD cod_cliente varchar(15) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD cod_subgerencia varchar(2) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD cod_region varchar(2) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD cod_zona varchar(4) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD cod_seccion varchar(1) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD cod_territorio varchar(6) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD direccion varchar(60) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD barrio varchar(30) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD desc_valor_venta varchar(15) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD campania_ingreso varchar(6) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD tipo_cliente varchar(3) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD cod_status_cliente varchar(2) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD clasificacion varchar(15) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD categoria varchar(3) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD nom_socia varchar(100) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD tel_socia varchar(100) NULL;
+
+ALTER TABLE kace2_belcorp.aux_clientes ADD promesa_entrega varchar(100) NULL;
